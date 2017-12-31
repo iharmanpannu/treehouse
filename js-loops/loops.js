cd60dd3b166fb81c6777c7c74d68c7004b157cea
iharmanpannu <iharmanpannu@gmail.com>
2017-12-30 20:17:53 -0800

    loops

--- js-loops/loops.js
+++ js-loops/loops.js
@@ -1,20 +1,20 @@
-let x = 1;
-do {
-  console.log("#" + x);
-  x += 2;
-} while (x <= 15);
+// let x = 1;
+// do {
+//   console.log("#" + x);
+//   x += 2;
+// } while (x <= 15);
 
 
-let js = 1;
-do {
-  console.log("I love JavaScript");
-  js += 1;
-} while (js <= 10);
+// let js = 1;
+// do {
+//   console.log("I love JavaScript");
+//   js += 1;
+// } while (js <= 10);
 
-do {
-  console.log("Hello");
-} while (false);
-console.log("Goodbye");
+// do {
+//   console.log("Hello");
+// } while (false);
+// console.log("Goodbye");
 
 // let counter = 0;
 
@@ -24,3 +24,11 @@ console.log("Goodbye");
 //   console.log( 'Hello');
 //   counter += 1;
 // }
+
+
+var secret = "What is the secret password?";
+
+do { secret !== "sesame"
+
+} while ( prompt(secret));
+document.write("You know the secret password. Welcome.");
