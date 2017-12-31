adc065bddd57dcaa0d0c41737681d12ade0b5577
iharmanpannu <iharmanpannu@gmail.com>
2017-12-30 19:47:01 -0800

    loops

--- js-loops/loops.js
+++ js-loops/loops.js
@@ -3,3 +3,15 @@ do {
   console.log("#" + x);
   x += 2;
 } while (x <= 15);
+
+
+let js = 1;
+do {
+  console.log("I love JavaScript");
+  js += 1;
+} while (js <= 10);
+
+do {
+  console.log("Hello");
+} while (false);
+console.log("Goodbye");
