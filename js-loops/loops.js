29222f08d66cb10d42c273d3149cd15c8331703e
iharmanpannu <iharmanpannu@gmail.com>
2017-12-30 19:52:33 -0800

    loops

--- js-loops/loops.js
+++ js-loops/loops.js
@@ -15,3 +15,12 @@ do {
   console.log("Hello");
 } while (false);
 console.log("Goodbye");
+
+// let counter = 0;
+
+// This creates a endless loop
+
+// while (counter >= 0) {
+//   console.log( 'Hello');
+//   counter += 1;
+// }
