868abe4f7ec93299a809679fa1d8256db696d5fd
iharmanpannu <iharmanpannu@gmail.com>
2018-01-03 10:59:19 -0800

    added functionality

--- js-loops/randomColors.js
+++ js-loops/randomColors.js
@@ -1,18 +1,49 @@
-let html = "";
-let red;
-let green;
-let blue;
-let rgbColor;
+// let html = "";
+// let rgbColor;
 
-function randomColors() {
-  for (let i = 1; i <= 10; i++) {
-    red = Math.floor(Math.random() * 256);
-    green = Math.floor(Math.random() * 256);
-    blue = Math.floor(Math.random() * 256);
-    rgbColor = `rgb(${red},${green},${blue})`;
-    html += `<div style="background-color:${rgbColor}"></div>`;
+// function randomRGB() {
+//   return Math.floor(Math.random() * 256);
+// }
+
+// function randomColor() {
+//   let color = `rgb(${randomRGB}, ${randomRGB}, ${randomRGB})`;
+//   return color;
+// }
+
+// function randomColors() {
+//   for (let i = 1; i <= 10; i++) {
+//     rgbColor = `rgb(${randomRgb()},${randomRgb()},${randomRgb()})`;
+//     html += `<div style="background-color:${rgbColor}"></div>`;
+//   }
+//   document.write(html);
+// }
+
+// randomColors();
+
+
+// function random() {
+//   for(let i = 2; i <= 24; i = i + 2)
+//   console.log(i);
+// }
+// random();
+
+  for (let i = 2; i <= 24; i = i + 2) {
+    console.log(i);
   }
-  document.write(html);
+ 
+
+var playList = ["I Did It My Way", "Respect", "Imagine", "Born to Run", "Louie Louie", "Maybellene"];
+
+function print(message) {
+  document.write(message);
 }
 
-randomColors();
+function printList(list) {
+  var htmlList = '<ul>';
+  for( var i = 0; i < list.lenght; i++ ) {
+    htmlList+= `<li> ${list[i]} </li>`;
+    htmlList+= "</ul>";
+  }
+  print(htmlList);
+}
+printList(playList);
