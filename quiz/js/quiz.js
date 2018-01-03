1b5f849e1d99e5df0a8bb6f7acda7e6e67dcb998
iharmanpannu <iharmanpannu@gmail.com>
2018-01-03 12:30:47 -0800

    changed the funcrionallity

--- quiz/js/quiz.js
+++ quiz/js/quiz.js
@@ -2,27 +2,27 @@ function print(message) {
   document.write(message);
 }
 
-function askQuestion(answers) {
-  let questions = [
-  ['President Of USA? '],
-  ['Last name of Barrack? ']
-  ['National animal of India? ']
+
+var questions = [
+  ['President Of USA? ', 'trump'],
+  ['Last name of Barrack? ', 'obama']
+  ['National animal of India? ','tiger']
 ];
-  let firstQuestion =  questions[0][0];
-  let secondQuestion = questions[1][0];
-  let thirdQuestion = questions[2][0];
 
-  if (prompt(firstQuestion) === ('trump' || 'Trump')) {
-    console.log(`<p>You answer is right!</p>`);
-  } else {
-    console.log(`<p>Wrong! Try Again</p>`);
-  }
+var correctAnswer = 0;
+var question;
+var answers;
+var html;
+var response;
 
-  if (prompt(secondQuestion) === "obama") {
-    console.log(`<p>You answer is right!</p>`);
-  } else {
-    console.log(`<p>Wrong! Try Again</p>`);
+for ( var i = 0; i < questions.length; i++ ) {
+  question = prompt(questions[i][0]);
+  answers = questions[i][1];
+  response = prompt(question);
+  if ( question === answers ) {
+    correctAnswer++;
   }
-  
 }
-askQuestion();
+
+html = `You got ${correctAnswer} ${question} right! `
+print(html);
