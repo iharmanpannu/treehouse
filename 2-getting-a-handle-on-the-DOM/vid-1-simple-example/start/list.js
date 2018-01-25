83f239ef107a34c1fb8133eca9bd94ba4b9d0a01
iharmanpannu <iharmanpannu@gmail.com>
2018-01-25 00:17:43 -0800

    select elements

--- 2-getting-a-handle-on-the-DOM/vid-1-simple-example/start/list.js
+++ 2-getting-a-handle-on-the-DOM/vid-1-simple-example/start/list.js
@@ -9,3 +9,10 @@ const errorNotPurple = document.getElementsByClassName("error-not-purple");
 for (let i = 0; i < errorNotPurple.length; i++) {
     errorNotPurple[i].style.color = "#000";
 }
+
+
+// ways to select elements 
+
+let navigationLinks = document.querySelectorAll("nav a");
+let galleryLinks = document.querySelectorAll("#gallery a");
+let footerImages = document.querySelectorAll("footer img");
