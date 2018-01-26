7e3b7d3f0b0f4b49fa450ae07de157850ce64ad4
iharmanpannu <iharmanpannu@gmail.com>
2018-01-26 00:20:44 -0800

    learned munda

--- 4-responding-to-user-interaction/vid-2-functions-as-parameters/start/app.js
+++ 4-responding-to-user-interaction/vid-2-functions-as-parameters/start/app.js
@@ -6,39 +6,51 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
+const listItems = document.querySelectorAll('li')
 
 toggleList.addEventListener('click', () => {
-  if (listDiv.style.display == 'none') {
-    toggleList.textContent = 'Hide list';
-    listDiv.style.display = 'block';
-  } else {
-    toggleList.textContent = 'Show list';                        
-    listDiv.style.display = 'none';
-  }                         
+    if (listDiv.style.display == 'none') {
+        toggleList.textContent = 'Hide list';
+        listDiv.style.display = 'block';
+    } else {
+        toggleList.textContent = 'Show list';
+        listDiv.style.display = 'none';
+    }
 });
 
 descriptionButton.addEventListener('click', () => {
-  descriptionP.innerHTML = descriptionInput.value + ':';
-  descriptionInput.value = '';
+    descriptionP.innerHTML = descriptionInput.value + ':';
+    descriptionInput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.createElement('li');
-  li.textContent = addItemInput.value;
-  ul.appendChild(li);
-  addItemInput.value = '';
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = document.createElement('li');
+    li.textContent = addItemInput.value;
+    ul.appendChild(li);
+    addItemInput.value = '';
 });
-  
+
 removeItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.querySelector('li:last-child');
-  ul.removeChild(li);
-});  
-  
-  
-  
-  
-  
-  
-  
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = document.querySelector('li:last-child');
+    ul.removeChild(li);
+});
+
+// listItems.addEventListener("mouseover", () => {
+//     listItems.style.textTransform = 'uppercase';
+// })
+
+// listItems.addEventListener("mouseout", () => {
+//     listItems.style.textTransform = "lowercase";
+// }) 
+
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener('mouseover', () => {
+        listItems[i].textContent = listItems[i].textContent.toUpperCase();
+
+        listItems[i].addEventListener("mouseout", () => {
+            listItems[i].textContent = listItems[i].textContent.toLowerCase();
+        })
+    })
+}
--- 4-responding-to-user-interaction/vid-2-functions-as-parameters/start/temp.js
+++ 4-responding-to-user-interaction/vid-2-functions-as-parameters/start/temp.js
@@ -1,9 +1,11 @@
-function exec(func, arg) {
-    func(arg);
-}
+// function exec(func, arg) {
+//     func(arg);
+// }
 
-exec((something) => console.log(something),
-    'Greetings, everyone!');
+window.setTimeout((something) => {
+        console.log(something);
+    },
+    3000, 'Greetings, everyone!');
 
 
 // function hello(func, arg) {
