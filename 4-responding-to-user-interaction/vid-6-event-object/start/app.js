abc8485b3a37bd20f6afe79432babd8ae47f602f
iharmanpannu <iharmanpannu@gmail.com>
2018-01-27 11:24:08 -0800

    changes

--- 4-responding-to-user-interaction/vid-6-event-object/start/app.js
+++ 4-responding-to-user-interaction/vid-6-event-object/start/app.js
@@ -1,48 +1,44 @@
-const toggleList = document.getElementById('toggleList');
-const listDiv = document.querySelector('.list');
-const descriptionInput = document.querySelector('input.description');
-const descriptionP = document.querySelector('p.description');
-const descriptionButton = document.querySelector('button.description');
-const addItemInput = document.querySelector('input.addItemInput');
-const addItemButton = document.querySelector('button.addItemButton');
-const removeItemButton = document.querySelector('button.removeItemButton');
+const toggleList = document.getElementById("toggleList");
+const listDiv = document.querySelector(".list");
+const descriptionInput = document.querySelector("input.description");
+const descriptionP = document.querySelector("p.description");
+const descriptionButton = document.querySelector("button.description");
+const addItemInput = document.querySelector("input.addItemInput");
+const addItemButton = document.querySelector("button.addItemButton");
+const removeItemButton = document.querySelector("button.removeItemButton");
 
-listDiv.addEventListener('mouseover', () => {
-  listItems[i].textContent = listItems[i].textContent.toUpperCase();
+listDiv.addEventListener("mouseover", e => {
+  e.target.listItems.textContent = listItems.textContent.toUpperCase();
 });
-listDiv.addEventListener('mouseout', () => {
-  listItems[i].textContent = listItems[i].textContent.toLowerCase();
+listDiv.addEventListener("mouseout", e => {
+  listItems.textContent = listItems.textContent.toLowerCase();
 });
 
-toggleList.addEventListener('click', () => {
-  if (listDiv.style.display == 'none') {
-    toggleList.textContent = 'Hide list';
-    listDiv.style.display = 'block';
+toggleList.addEventListener("click", () => {
+  if (listDiv.style.display == "none") {
+    toggleList.textContent = "Hide list";
+    listDiv.style.display = "block";
   } else {
-    toggleList.textContent = 'Show list';                        
-    listDiv.style.display = 'none';
-  }                         
+    toggleList.textContent = "Show list";
+    listDiv.style.display = "none";
+  }
 });
 
-descriptionButton.addEventListener('click', () => {
-  descriptionP.innerHTML = descriptionInput.value + ':';
-  descriptionInput.value = '';
+descriptionButton.addEventListener("click", () => {
+  descriptionP.innerHTML = descriptionInput.value + ":";
+  descriptionInput.value = "";
 });
 
-addItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.createElement('li');
+addItemButton.addEventListener("click", () => {
+  let ul = document.getElementsByTagName("ul")[0];
+  let li = document.createElement("li");
   li.textContent = addItemInput.value;
   ul.appendChild(li);
-  addItemInput.value = '';
+  addItemInput.value = "";
 });
-  
-removeItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.querySelector('li:last-child');
+
+removeItemButton.addEventListener("click", () => {
+  let ul = document.getElementsByTagName("ul")[0];
+  let li = document.querySelector("li:last-child");
   ul.removeChild(li);
 });
-  
-  
-  
-  
