96371167254cc7fa4370adfd638fa23187fb2e0a
iharmanpannu <iharmanpannu@gmail.com>
2018-01-28 10:49:33 -0800

    adding edit button

--- 1-adding-and-removing-names/1-tour-the-application/start/app.js
+++ 1-adding-and-removing-names/1-tour-the-application/start/app.js
@@ -1,23 +1,27 @@
 const form = document.getElementById("registrar");
 const input = document.querySelector("input");
 const ul = document.getElementById("invitedList");
+const li = document.createElement("li");
 
 //This function cretes a list Items
 function createLI(text) {
-  const li = document.createElement("li");
   const button = document.createElement("button");
   const label = document.createElement("label");
   const checkBox = document.createElement("input");
+  const editButton = document.createElement("button");
 
   li.textContent = text;
-
+  // Item Label
   label.textContent = "Confirmed";
   label.appendChild(checkBox);
   li.appendChild(label);
-
+  // Check Box
   checkBox.type = "checkBox";
   checkBox.className = "checkBox";
-
+  // Edit Button
+  editButton.textContent = "Edit";
+  li.appendChild(editButton);
+  //Remove Button
   button.textContent = "Remove";
   li.appendChild(button);
   return li;
@@ -30,7 +34,7 @@ form.addEventListener("submit", e => {
   const li = createLI(text);
   ul.appendChild(li);
 });
-
+// Class to change color on checkBox Clicked
 ul.addEventListener("change", e => {
   const checkbox = e.target;
   const checked = checkbox.checked;
@@ -40,12 +44,22 @@ ul.addEventListener("change", e => {
     : (listItem.className = "");
 });
 
+// Remove Button
 ul.addEventListener("click", e => {
   if (event.target.tagName === "BUTTON") {
     if (event.target.textContent === "Remove") {
       const li = e.target.parentNode;
       const ul = li.parentNode;
       ul.removeChild(li);
+    }
+  }
+});
+
+// Edit Button
+ul.addEventListener("click", e => {
+  if (event.target.tagName === "BUTTON") {
+    if (event.target.textContent === "Edit") {
+      li.textContent = input.value;
     }
   }
 });
