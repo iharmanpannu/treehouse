c0226adeb09f3595a48bb752249ddacbd5d732ac
iharmanpannu <iharmanpannu@gmail.com>
2018-01-29 13:17:18 -0800

    box

--- jQuery/projectBox/js/app.js
+++ jQuery/projectBox/js/app.js
@@ -1,3 +1,14 @@
+const box = document.querySelector(".box");
+
 //jQuery('.box').hide();
-$('.box').hide();
-$('.box').show();
+// $('.box').hide();
+// $('.box').show();
+
+// box.addEventListener("click", () => {
+//   alert("You clicked me ");
+//   // const button = document.createElement('button');
+// });
+
+$(".box").click(function() {
+  alert("Hello World");
+});
