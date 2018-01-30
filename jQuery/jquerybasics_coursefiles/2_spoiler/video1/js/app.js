a4123487da18a67c825a80b6950d22ce7c8e8030
iharmanpannu <iharmanpannu@gmail.com>
2018-01-30 00:32:33 -0800

    building a project

--- jQuery/jquerybasics_coursefiles/2_spoiler/video1/js/app.js
+++ jQuery/jquerybasics_coursefiles/2_spoiler/video1/js/app.js
@@ -1,5 +1,19 @@
+// Reveal Spoiler button
+const $button = $("<button>Reveal Spoiler</button>");
+$(".spoiler").append($button);
 
+//Hide the Spoiler
+$(".spoiler span").hide();
 
+// When the button is pressed
 
-
-
+$(".spoiler button").click(() => {
+  // Show the Spoiler
+  $(".spoiler span")
+    .fadeIn(500)
+    .show();
+  //Hide the 'Reveal Spoiler' Button
+  $(".spoiler button")
+    .fadeOut(1000)
+    .hide();
+});
