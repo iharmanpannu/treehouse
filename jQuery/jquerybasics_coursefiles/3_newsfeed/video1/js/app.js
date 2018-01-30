039388eeb6807ca80b869dd64d6d9a7e4a86c0e3
iharmanpannu <iharmanpannu@gmail.com>
2018-01-30 14:04:09 -0800

    adding methods

--- jQuery/jquerybasics_coursefiles/3_newsfeed/video1/js/app.js
+++ jQuery/jquerybasics_coursefiles/3_newsfeed/video1/js/app.js
@@ -1,6 +1,18 @@
 const $odd = $("a:odd");
 const $secureLinks = $('a[href^="https://"]');
-const $pdfLinks = $('a[href$=".pdf"]');
-// $pdfLinks.hide();
-$secureLinks.attr("target", "_blank");
-$pdfLinks.attr("download", true);
+const $pdfs = $('a[href$=".pdf"]');
+const $checkBox = $("input").attr("checkbox");
+
+// Add Attributes & Add Classes
+$secureLinks.attr("target", "_blank").addClass("secure");
+$pdfs.attr("download", false).addClass("pdf");
+
+$pdfs.on("click", e => {
+  //Check to see if the checkbox is checked
+  e.preventDefault();
+  // If no prevent user from downloading
+  if ($($checkBox).is(":checked")) {
+    $pdfs.attr("download", true);
+  }
+  // If yes allow user to download
+});
