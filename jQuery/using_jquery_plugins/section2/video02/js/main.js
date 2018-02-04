0be8b273619d6c56387b7b0020ea28809c85cb43
iharmanpannu <iharmanpannu@gmail.com>
2018-02-04 13:02:55 -0800

    added navsticky

--- jQuery/using_jquery_plugins/section2/video02/js/main.js
+++ jQuery/using_jquery_plugins/section2/video02/js/main.js
@@ -6,4 +6,18 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
-$(".header".sticky({ topSpacing: 0 }));
+$(".header").sticky();
+$(".header").on("sticky-start", () => {
+  $(".description").html("We build <strong>great</strong> apps.");
+});
+$(".header").on("sticky-end", () => {
+  $(".description").html("We build apps");
+});
+
+$(".emailus").sticky({ topSpacing: 60 });
+$(".emailus").on("sticky-start", () => {
+  $(this).append(' <a class="email-text"> Email us </a> ');
+});
+$(".emailus").on("sticky-end", () => {
+  $(".email-text").remove();
+});
