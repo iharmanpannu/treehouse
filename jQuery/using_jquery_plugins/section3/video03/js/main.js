af94221393c7d0fdca4e87e673755e2e0804dd93
iharmanpannu <iharmanpannu@gmail.com>
2018-02-04 13:14:26 -0800

    learning about slick plugin

--- jQuery/using_jquery_plugins/section3/video03/js/main.js
+++ jQuery/using_jquery_plugins/section3/video03/js/main.js
@@ -1,57 +1,42 @@
 $(".animsition").animsition({
-  inClass: 'fade-in-right-lg',
-  outClass: 'fade-out-right-lg',
-  linkElement: 'header a',
+  inClass: "fade-in-right-lg",
+  outClass: "fade-out-right-lg",
+  linkElement: "header a",
   inDuration: 1000,
   outDuration: 500
 });
 
-$('.header').sticky({
-  getWidthFrom: '.container',
+$(".header").sticky({
+  getWidthFrom: ".container",
   responsiveWidth: true
 });
 
-$('.header').on('sticky-start', function () {
-  $('.description').html('We build <strong>great</strong> apps');
+$(".header").on("sticky-start", function() {
+  $(".description").html("We build <strong>great</strong> apps");
 });
 
-$('.header').on('sticky-end', function () {
-  $('.description').html('We build apps');
+$(".header").on("sticky-end", function() {
+  $(".description").html("We build apps");
 });
 
-$('.work').sticky({
+$(".work").sticky({
   topSpacing: 60,
-  getWidthFrom: '.container',
+  getWidthFrom: ".container",
   responsiveWidth: true
 });
-$('.work').on('sticky-start', function() {
-  $(this).append(' <a href="mailto:email@website.com" class="email-text">Email&nbsp;us</a>');
+$(".work").on("sticky-start", function() {
+  $(this).append(
+    ' <a href="mailto:email@website.com" class="email-text">Email&nbsp;us</a>'
+  );
 });
-$('.work').on('sticky-end', function() {
-    $('.email-text').remove();
+$(".work").on("sticky-end", function() {
+  $(".email-text").remove();
 });
 
+// Slick animations
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(".slides").slick({
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 1000
+});
