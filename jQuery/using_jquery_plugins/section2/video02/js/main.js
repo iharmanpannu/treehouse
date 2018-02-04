a83fc8292c19ba8ebfe83e045221a4cf1012d55a
iharmanpannu <iharmanpannu@gmail.com>
2018-02-04 11:30:38 -0800

    added js

--- jQuery/using_jquery_plugins/section2/video02/js/main.js
+++ jQuery/using_jquery_plugins/section2/video02/js/main.js
@@ -1,8 +1,9 @@
 $(".animsition").animsition({
-  inClass: 'fade-in-right-lg',
-  outClass: 'fade-out-right-lg',
-  linkElement: 'header a',
+  inClass: "fade-in-right-lg",
+  outClass: "fade-out-right-lg",
+  linkElement: "header a",
   inDuration: 1000,
   outDuration: 500
 });
 
+$(".header".sticky({ topSpacing: 0 }));
