f08b3b9087e384c9eb13fb1b0921c0b787fec1f2
iharmanpannu <iharmanpannu@gmail.com>
2018-02-06 21:45:41 -0800

    added fcuntion

--- JavaScript/1-adding-and-removing-names/1-tour-the-application/final/app.js
+++ JavaScript/1-adding-and-removing-names/1-tour-the-application/final/app.js
@@ -1,12 +1,12 @@
 const form = document.getElementById("registrar");
-
+const ul = document.getElementById("invitedList");
 const input = form.querySelector("input");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
   const text = input.value;
   input.value = "";
-  const ul = document.getElementById("invitedList");
+
   const li = document.createElement("li");
   li.textContent = text;
   ul.appendChild(li);
@@ -15,7 +15,36 @@ form.addEventListener("submit", e => {
   label.textContent = "Confirmed";
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-
   label.appendChild(checkbox);
   li.appendChild(label);
+
+  const remove = document.createElement("button");
+  remove.textContent = "Remove";
+  li.appendChild(remove);
+
+  //   const editBtn = document.createElement("button");
+  //   editBtn.textContent = "edit";
+  //   li.appendChild(editBtn);
+
+  //   editBtn.addEventListener("click", () => {});
+});
+
+ul.addEventListener("change", e => {
+  const checkbox = e.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+
+  if (checked) {
+    listItem.className = "responded";
+  } else {
+    listItem.className = "";
+  }
+});
+
+ul.addEventListener("click", e => {
+  if (e.target.tagName === "BUTTON") {
+    const li = e.target.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
+  }
 });
