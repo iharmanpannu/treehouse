cf7b40aeccc25ca961483aa93e91314e8a008428
iharmanpannu <iharmanpannu@gmail.com>
2018-02-06 21:03:03 -0800

    dom manipulation

---
+++ JavaScript/1-adding-and-removing-names/1-tour-the-application/final/app.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("registrar");
+
+const input = form.querySelector("input");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = "";
+  const ul = document.getElementById("invitedList");
+  const li = document.createElement("li");
+  li.textContent = text;
+  ul.appendChild(li);
+
+  const label = document.createElement("label");
+  label.textContent = "Confirmed";
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  label.appendChild(checkbox);
+  li.appendChild(label);
+});
