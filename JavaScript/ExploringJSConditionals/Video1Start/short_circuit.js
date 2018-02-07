2f4d613667e5a87e8c62ada4e658334dad598d60
iharmanpannu <iharmanpannu@gmail.com>
2018-02-07 14:17:44 -0800

    learning syntax

--- JavaScript/ExploringJSConditionals/Video1Start/short_circuit.js
+++ JavaScript/ExploringJSConditionals/Video1Start/short_circuit.js
@@ -1,2 +1,15 @@
-console.log(3 === 3 && "cow" && false);
-console.log(3 === 3 || "cow");
+// console.log(3 === 3 && "cow" && false);
+// console.log(3 === 3 || "cow");
+
+// function isAdult(age) {
+//   return age && age >= 18;
+// }
+// console.log(isAdult(20));
+
+function countToFive(start) {
+  start = start || 1;
+  for (let i = start; i < 5; i += 1) {
+    console.log(i);
+  }
+}
+countToFive(0);
