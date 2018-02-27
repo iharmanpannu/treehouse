6dfde618860594dbbf3c9cac8b94c7b71c9f2b2a
iharmanpannu <iharmanpannu@gmail.com>
2018-02-27 12:17:41 -0800

    adding room list

--- JavaScript/Ajax/stage2/video6/js/widget.js
+++ JavaScript/Ajax/stage2/video6/js/widget.js
@@ -1,20 +1,33 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
-      } else {
-        statusHTML += '<li class="out">';
-      }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
+function showList() {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const employees = JSON.parse(xhr.responseText);
+      let status = `<ul class="bulleted">`;
+      employees.forEach(employee => {
+        if (employee.inoffice === true) {
+          status += `<li class="in">`;
+        } else {
+          status += `<li class="out">`;
+        }
+        status += employee.name;
+        status += `</li>`;
+      });
+      status += `</ul>`;
+      const div = document.getElementById("employeeList");
+      div.innerHTML = status;
     }
-    statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
-};
-xhr.open('GET', '../data/employees.json');
-xhr.send();
+  };
+  xhr.open("GET", "data/employees.json", true);
+  xhr.send();
+}
+
+showList();
+
+function showRooms() {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    const rooms = JSON.parse(xhr.responseText);
+  };
+  xhr.open("GET", "data/rooms.js");
+}
