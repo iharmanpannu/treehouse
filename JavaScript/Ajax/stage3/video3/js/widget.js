908c169ad67ad157a7c788b167dfc67b282c55dd
iharmanpannu <iharmanpannu@gmail.com>
2018-02-28 13:33:35 -0800

    added let

--- JavaScript/Ajax/stage3/video3/js/widget.js
+++ JavaScript/Ajax/stage3/video3/js/widget.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  const url = "..data/employees.json";
+  const url = "..data/employees.json"; // url file
 
-  $.getJSON(url, function(response) {});
+  $.getJSON(url, function(response) {
+    let status = `<u class="bulleted">`;
+  }); // end json
 }); // End ready
