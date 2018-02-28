bcbc1b663862dc4107eabe8c89a917547837c03d
iharmanpannu <iharmanpannu@gmail.com>
2018-02-28 13:37:24 -0800

    changes

--- JavaScript/Ajax/stage3/video3/js/widget.js
+++ JavaScript/Ajax/stage3/video3/js/widget.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   const url = "..data/employees.json"; // url file
 
   $.getJSON(url, function(response) {
-    let status = `<u class="bulleted">`;
+    let status = `<ul class="bulleted">`;
+    $.each(response, function(index, employee) {
+      if (employee.inoffice === true) {
+        status += `<li class="in">`;
+      } else {
+        status += `<li class="out">`;
+      }
+    });
   }); // end json
 }); // End ready
