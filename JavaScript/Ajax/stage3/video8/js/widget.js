b49fbf52272f2805f0145bfe86fceea2bdaee72b
Harman Pannu <iharmanpannu@gmail.com>
2018-03-03 16:04:30 -0800

    finished

--- JavaScript/Ajax/stage3/video8/js/widget.js
+++ JavaScript/Ajax/stage3/video8/js/widget.js
@@ -12,6 +12,18 @@ $(document).ready(function() {
     statusHTML += "</ul>";
     $("#employeeList").html(statusHTML);
   }); // end getJSON
-  const $url = "../data/rooms.json";
-  $.getJSON();
+  const $url = "data/rooms.json";
+  $.getJSON($url, function(data) {
+    let status = `<ul class="rooms">`;
+    $.each(data, function(index, isAvailable) {
+      if (isAvailable.available === true) {
+        status += `<li class="full">`;
+      } else {
+        status += `<li class="empty">`;
+      }
+      status += `${isAvailable.room}</li>`;
+    });
+    status += `</ul>`;
+    $("#roomList").html(status);
+  });
 }); // end ready
