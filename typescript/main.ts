af795e579be35c116c9cb7b6c96d64bdfaa636d5
Harman Pannu <iharmanpannu@gmail.com>
2018-03-04 17:26:45 -0800

    learning constructor

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,4 +1,20 @@
-let doLog = message => {
-  message = "Hi";
-  console.log(message);
-};
+class Point {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    //   constructor(x?: number, y?: number) { this makes it optional
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    console.log(`X: ${this.x} Y: ${this.y}`);
+  }
+
+  getDistance(another: Point) {
+    // ..
+  }
+}
+let point = new Point(1, 2);
+// point.x = 1;
+// point.y = 2;
+point.draw();
