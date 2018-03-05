9582266e4220ddad29811d10f636d60622722f6a
Harman Pannu <iharmanpannu@gmail.com>
2018-03-04 16:39:51 -0800

    learning typescript

--- typescript/main.js
+++ typescript/main.js
@@ -1,13 +1,4 @@
-// let message;
-// message = "abc";
-// // let endswith = message.endsWith("c");
-// let alternativeway = (message as string).endsWith("c");
-// // message = 5;
-// console.log(alternativeway);
-var log = function (message) {
-    console.log(message);
-};
 var doLog = function (message) {
-    message = "hello";
+    message = "Hi";
     console.log(message);
 };
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,14 +1,4 @@
-// let message;
-// message = "abc";
-// // let endswith = message.endsWith("c");
-// let alternativeway = (message as string).endsWith("c");
-// // message = 5;
-// console.log(alternativeway);
-
-// let log = function(message) {
-//   console.log(message);
-// };
 let doLog = message => {
-  message = "hello";
+  message = "Hi";
   console.log(message);
 };
