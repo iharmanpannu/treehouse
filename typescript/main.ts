d250770299ecba87b5b397e57a20eecabf62b9d1
Harman Pannu <iharmanpannu@gmail.com>
2018-03-04 20:19:18 -0800

    init

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,21 +1,40 @@
 class Point {
-  x: number;
+  //   x: number;
   //   private x: number; this makes it private
-  y: number;
-  constructor(x: number, y: number) {
+  //   y: number;
+  constructor(private x?: number, private y?: number) {
     //   constructor(x?: number, y?: number) { this makes it optional
-    this.x = x;
-    this.y = y;
+    // this.x = x;
+    // this.y = y;
   }
   draw() {
     console.log(`X: ${this.x} Y: ${this.y}`);
   }
 
-  getDistance(another: Point) {
-    // ..
+  //   getDistance(another: Point) {
+  //     // ..
+  //   }
+  //   getX() {
+  //     return this.x;
+  //   }
+
+  //   setX(value) {
+  //     if (value < 0) {
+  //       throw new Error("value cannot be less than  0.");
+  //     }
+  //   }
+
+  // Instead of code above we can use
+  get X() {
+    return this.x;
+  }
+  set X(value) {
+    if (value < 0) throw new Error("value cannot be less than  0.");
   }
 }
 let point = new Point(1, 2);
 // point.x = 1;
 // point.y = 2;
+let x = point.getX();
+point.setX(10);
 point.draw();
