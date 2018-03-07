9e48ca460a558a2728c003cffc638370ef202b28
Harman Pannu <iharmanpannu@gmail.com>
2018-03-07 01:08:20 -0800

    added list

--- Angular-treehouse/th-ang/src/app/app.component.ts
+++ Angular-treehouse/th-ang/src/app/app.component.ts
@@ -14,3 +14,19 @@ export class AppComponent {
     this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)];
   }
 }
+
+// Difference between "=" sign and "==" sign and "===" sign
+
+// use "=" to assign a value to value
+
+let hello = "Hello";
+
+// By using "==" you check if something is equal to something else.
+// "==" only compares value not type
+
+1 == "1" // true
+
+// By using === you check if something is equal to something else.
+//  "===" compares the value and type
+1 === 1 // true
+1 === "1" // false
