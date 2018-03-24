fa5c199e31830de06ca3955818fc6d98081c4967
Harman Pannu <iharmanpannu@gmail.com>
2018-03-24 11:41:39 -0700

    adding another function

--- algo/exercises/anagrams/index.js
+++ algo/exercises/anagrams/index.js
@@ -8,7 +8,7 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
+function anagrams(stringA, stringb) {
   const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
@@ -24,9 +24,10 @@ function anagrams(stringA, stringB) {
 
 function buildCharMap(str) {
   const charMap = {};
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase) {
     charMap[char] = charMap[char] + 1 || 1;
   }
+  return charMap;
 }
 
 // console.log()
